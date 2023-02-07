4747d90f5582f5b734efafd06945b4ea044a2404
Avi-J0506 <92037313+Avi-J0506@users.noreply.github.com>
2023-02-08 02:55:30 +0530

    api integration

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './sidebar.css'
 import SidebarButton from './sidebarButton'
 import {MdFavorite} from "react-icons/md"
@@ -6,11 +6,20 @@ import {FaGripfire,FaPlay} from "react-icons/fa"
 import {FaSignOutAlt} from "react-icons/fa"
 import {IoLibrary} from "react-icons/io5"
 import {MdSpaceDashboard} from "react-icons/md"
+import apiClient from '../../spotify'
 
 export default function Sidebar() {
+  const [image,setImage] = useState(
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDPV7ZNTxIDr9rmQ0filsOZppA_vL5-5AUKg&usqp=CAU"
+  );
+  useEffect(()=>{
+    apiClient.get("me").then(response => {
+      console.log(response.data.images[0].url);
+    });
+  },);
   return (
     <div className='sidebar-container'>
-      <img src='./' className='profile-image' alt='profile' />
+      <img src={image} className='profile-image' alt='profile' />
       <div>
         <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard/>}/>
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire/>}/>
--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Feed from "../Feed/feed";
 import Library from "../Library/library";
@@ -7,9 +7,31 @@ import Player from "../Player/player";
 import Favorites from "../favorites/favorites";
 import './home.css'
 import Sidebar from '../../components/sidebar';
+import Login from '../auth/login';
+import { setClientToken } from '../../spotify';
 
 export default function Home() {
-  return (
+
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if(token && hash){
+      const _token = hash.split('&')[0].split("=")[1];
+      window.localStorage.setItem("token",_token);
+      setToken(_token);
+      setClientToken(_token)
+    }
+    else{
+      setToken(token);
+      setClientToken(token)
+    }
+  },[]);
+  return !token ? (
+    <Login/>
+  ):(
     <Router>
         <div className='main-body'>
           <Sidebar/>
@@ -22,5 +44,5 @@ export default function Home() {
         </Routes>
         </div>
     </Router>
-  )
+  );
 }
